53a88e82468b123b6f17adc9c38fb99c3c1c16bb
hskkou <hskk1120551@gmail.com>
2018-08-18 21:12:46 +0900

    初期化

---
+++ TRPG3D/Assets/Scripts/Game/Managers/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+	/// <summary>
+	/// ゲーム管理
+	/// </summary>
+	public class GameManager : MonoBehaviour
+	{
+		
+	}
+}
